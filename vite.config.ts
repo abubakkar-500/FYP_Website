a9de201e0400d2011cc01af45ead07c4109cf723
abu bakkar <21pwcse2004@uetpeshawar.edu.pk>
2025-05-24 15:37:51 +0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FYP_Project_Website/',  // Use your repo name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
